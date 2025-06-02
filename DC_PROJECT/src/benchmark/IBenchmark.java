f46ecf47d34cf2120691851ff0beb0c723857fdd
ally-pilots <alexandra-monica.rusu@student.upt.ro>
2025-06-03 00:35:40 +0300

    Delete DC_PROJECT/src directory

--- DC_PROJECT/src/benchmark/IBenchmark.java
+++
@@ -1,9 +0,0 @@
-package benchmark;
-
-public interface IBenchmark {
-    void initialize(Object... params);
-    void run();
-    void run(Object... options);
-    void clean();
-    void cancel();
-}
--- DC_PROJECT/src/logging/ILogger.java
+++
@@ -1,9 +0,0 @@
-package logging;
-
-public interface ILogger {
-    void write(String s);
-    void write(long l);
-    void write(Object... values);
-    void writeTime(String message, long timeNs, TimeUnit unit);
-    void close();
-}
